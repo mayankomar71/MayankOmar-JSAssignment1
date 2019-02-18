f2d0eaf726f0432a4f215d5475d32a05fe914858
Mayank Omar <MayankOmar71@gmail.com>
2019-02-18 17:15:25 +0530

    JS Assignment1

---
+++ Anagram1.js
@@ -0,0 +1,46 @@
+function ANAGRAM_CHECK(Str1) {
+    var str2 = Str1.toLowerCase();
+    var array = [];
+    for (var i = 0; i < str2.length; i++) {
+        array[i] = str2.charAt(i);
+    }
+
+    var tmp;
+    //sorting array after splitting it
+    for (var i = 0; i < array.length; i++) {
+        for (var j = i + 1; j < array.length; j++) {
+
+            if (array[i] > array[j]) {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+    //The join() method joins the sorted array  and returns the string.
+    var text = "";
+    for (var i = 0; i < array.length; i++) {
+        text += array[i];
+    }
+    return text;
+}
+//function call
+var Str1 = ANAGRAM_CHECK("Mayank Omar");
+var Str2 = ANAGRAM_CHECK("Mayank Omar");
+//calculating length of  Sorted Strings
+var length_Str1 = Str1.length;
+var length_Str2 = Str2.length;
+//camparing length of Sorted Strings 
+if (length_Str1 == length_Str2) {
+    //if both Strings are same will output it as Anagram
+    if (Str1 == Str2) {
+        console.log("String is Anagram");
+    }
+    //else it will output as not anagram
+    else {
+        console.log("String is not anagram");
+    }
+
+} else {
+    console.log("Strings are of different length");
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MayankOmar-JSAssignment1
+
+In this Assignment I have created a program  that checks if two words are anagrams.
